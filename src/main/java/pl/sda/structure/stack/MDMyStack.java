8f983cde30f87112a0ee3aa7c805514c108f2ca2
bk14er <mateduraj@gmail.com>
2019-01-12 15:55:58 +0100

    stack implementation

---
+++ src/main/java/pl/sda/structure/stack/MDMyStack.java
@@ -0,0 +1,52 @@
+package pl.sda.structure.stack;
+
+/**
+ * Stos to struktura LIFO(LAST IN , FIRST OUT)
+ * <p>
+ */
+public class MDMyStack<T> {
+
+    private class Node {
+        private T data;
+        private Node next;
+        private Node (T data) {
+            this.data = data;
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    /**
+     * Metoda odkłada na stos
+     * @param data
+     */
+    public void push(T data) {
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+        size++;
+    }
+
+    /**
+     * Metoda pobiera ze stosu i usuwa
+     * @return
+     */
+    public T pop() {
+        if(head == null){
+            return null;
+        }
+        Node temp = head;
+        head = head.next;
+        return temp.data;
+    }
+
+    public void printStack(){
+        //TODO wypisz wszystkie elemnety stosu (Node.data)
+    }
+
+    public int size() {
+        return size;
+    }
+
+}
