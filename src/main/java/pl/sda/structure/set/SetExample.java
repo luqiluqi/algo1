6c833a3adba6391c1d4f28e699ba2f449cfb4045
bk14er <mateduraj@gmail.com>
2019-01-13 12:03:00 +0100

    Set example

---
+++ src/main/java/pl/sda/structure/set/SetExample.java
@@ -0,0 +1,34 @@
+package pl.sda.structure.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+
+    public static void main(String args[]){
+        Set<String> zbior = new HashSet<>();
+        zbior.add("ala");
+        zbior.add("ma");
+        zbior.add("kota");
+        zbior.add("ala");
+
+        int size = zbior.size();
+        System.out.println(size);
+
+
+        Set<Person> personSet = new HashSet<>();
+
+        personSet.add(new Person("Monika"));
+        personSet.add(new Person("Ala"));
+        personSet.add(new Person("Monika"));
+
+        int size1 = personSet.size();
+        System.out.println(size1);
+
+
+
+
+    }
+
+
+}
