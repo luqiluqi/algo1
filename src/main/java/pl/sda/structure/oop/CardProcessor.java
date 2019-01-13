7767f57ce4989bad126c2469e78e528fcf57e262
bk14er <mateduraj@gmail.com>
2019-01-13 10:46:39 +0100

    card processor

---
+++ src/main/java/pl/sda/structure/oop/CardProcessor.java
@@ -0,0 +1,28 @@
+package pl.sda.structure.oop;
+
+public class CardProcessor {
+
+
+    /**
+     *
+     * Metoda zwraca :
+     *
+     * MASTERCARD - prefixy 34,35,36
+     *              długość 16,17,18
+     *              czyli np. 34 0000 1000 9890 111 (17 znaków)
+     *
+     * VISA      - prefixy 66, 65
+     *             długość 18
+     *             czyli np 66 0000 1000 9890 6511 (18 znaków)
+     *
+     * RUCARD     -  prefixy 90
+     *               długość 21,22,23
+     *               czyli np. 90 1000 0000 0110 5432 7652 (22 znaki)
+     *
+     *
+     */
+    public String getCardType(String number){
+        return "";
+    }
+
+}
