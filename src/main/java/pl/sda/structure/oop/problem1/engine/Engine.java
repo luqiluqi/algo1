7ed89e4414c02163e897d471af96093de79ffc06
bk14er <mateduraj@gmail.com>
2019-01-17 09:30:59 +0100

    engine + tyre

--- src/main/java/pl/sda/structure/oop/problem1/engine/Engine.java
+++ src/main/java/pl/sda/structure/oop/problem1/engine/Engine.java
@@ -1,4 +1,11 @@
 package pl.sda.structure.oop.problem1.engine;
 
 public class Engine {
+
+    // Odrzutowy/ Spalinowy / Elektryczny/ Hybrydowy
+    private String engineType;
+
+    // MOC 
+    private int power;
+
 }
--- src/main/java/pl/sda/structure/oop/problem1/tyre/Tyre.java
+++ src/main/java/pl/sda/structure/oop/problem1/tyre/Tyre.java
@@ -1,4 +1,16 @@
 package pl.sda.structure.oop.problem1.tyre;
 
 public class Tyre {
+
+    // Private int rozmiar w calach
+    private int size;
+
+    //marka opon
+    private String tyreBrandName;
+
+    // Typ opon : ZIMOWE/LETNIE/WIELOSEZONOWE
+    private String tyreType;
+
+
+
 }
