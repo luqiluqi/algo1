f63f061d116ac1295e7a881c2312c8722c1dda26
bk14er <mateduraj@gmail.com>
2019-01-17 10:10:28 +0100

    problem 2 extra subtask

---
+++ src/main/java/pl/sda/structure/oop/problem2/IntegerSimpleCalculator.java
@@ -0,0 +1,18 @@
+package pl.sda.structure.oop.problem2;
+
+/**
+ * Napisz 4 metody:
+ * <p>
+ * add(int toAddValue) - dodaje this.value + toAddValue
+ * subtract(int toSubtract) - odejmuje this.value - toSubtract
+ * multiply(int toMultiply) -  mnoży toMultiply * this.value
+ * divide(int toDivide) - dzieli this.value / toDivide
+ * <p>
+ * Metody muszą działać według wzorca "fluent api"
+ */
+public class IntegerSimpleCalculator {
+
+    private int value;
+
+
+}
---
+++ src/test/java/pl/sda/structure/oop/problem2/IntegerSimpleCalculatorTest.java
@@ -0,0 +1,44 @@
+package pl.sda.structure.oop.problem2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IntegerSimpleCalculatorTest {
+
+
+    @Test
+    public void testAdd(){
+
+
+
+        assertEquals(2,2);
+    }
+
+    @Test
+    public void testSubtract(){
+
+        assertEquals(2,2);
+    }
+
+    /**
+     * Test dla operacji mnożenia
+     */
+    @Test
+    public void testMultiply(){
+
+        assertEquals(2,2);
+    }
+
+    /**
+     * Test dla operacji dzielenia
+     */
+    @Test
+    public void testDivide(){
+
+        assertEquals(2,2);
+    }
+
+
+
+}
